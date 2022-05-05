612ca79c6313d0876bef94f26f827a486f5d593e
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-05-05 03:28:06 +0000

    added code

--- main.cs
+++ main.cs
@@ -9,27 +9,23 @@ class Program
 {
     public static void Main(string[] args)
     {
-        int num17;
-        int num13;
-        int num5;
-        Console.Write(" Figure out which movies the user is allowed to watch based on their age:");
-        Console.ReadLine();
-        Console.Write("Age groups: 17yrs old, 13yrs old, 5yrs old and 5yrs and younger");
-        Console.ReadLine();
-        Console.Write("Enter your age: ");
-      
-        num17 = Convert.ToInt32(Console.ReadLine());
-        num13 = Convert.ToInt32(Console.ReadLine());
-        num5 = Convert.ToInt32(Console.ReadLine());
-        if (num17 == 17)
-            Console.WriteLine("You can see an R-rated movie alone!");
-        else if (num13 == 13) 
-            Console.WriteLine("You can see a PG-13 rated movie alone!");
-        else if (num5 == 5)
-            Console.WriteLine("You can watch a G-rated movie alone");
-        else {
-            Console.WriteLine("You are too young to watch movies alone.");
-        }
-        Console.WriteLine("\nDone.");
-    }
-}
+
+    int age;
+
+    Console.WriteLine("This program determines which rated movies they are allowed to watch alone.");
+    Console.WriteLine("");
+    Console.WriteLine("Enter age: ");
+    age = Convert.ToInt32(Console.ReadLine());
+
+    if (age >= 17)
+        Console.WriteLine("You are allowed to watch an R-rated movie alone.");
+    else if (age >= 13)
+        Console.WriteLine("You are allowed to watch a PG-13 rated movie alone.");
+    else if (age >= 5)
+        Console.WriteLine("You are allowed to watch a G-rated movie alone.");
+    else 
+        Console.WriteLine("You are too young to watch movies alone.");
+
+    Console.WriteLine("/nDone.");
+  }
+} 
