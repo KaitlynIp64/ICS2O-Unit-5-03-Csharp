1e0928a8ee54b532fa7332975409876093238892
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-05-05 03:35:46 +0000

    fixed whitespace

--- main.cs
+++ main.cs
@@ -10,22 +10,22 @@ class Program
     public static void Main(string[] args)
     {
 
-    int age;
+   int age;
 
-    Console.WriteLine("This program determines which rated movies they are allowed to watch alone.");
-    Console.WriteLine("");
-    Console.WriteLine("Enter age: ");
-    age = Convert.ToInt32(Console.ReadLine());
+   Console.WriteLine("This program determines which rated movies they are allowed to watch alone.");
+   Console.WriteLine("");
+   Console.WriteLine("Enter age: ");
+   age = Convert.ToInt32(Console.ReadLine());
 
-    if (age >= 17)
-        Console.WriteLine("You are allowed to watch an R-rated movie alone.");
-    else if (age >= 13)
-        Console.WriteLine("You are allowed to watch a PG-13 rated movie alone.");
-    else if (age >= 5)
-        Console.WriteLine("You are allowed to watch a G-rated movie alone.");
-    else 
-        Console.WriteLine("You are too young to watch movies alone.");
+   if (age >= 17)
+    Console.WriteLine("You are allowed to watch an R-rated movie alone.");
+   else if (age >= 13)
+     Console.WriteLine("You are allowed to watch a PG-13 rated movie alone.");
+   else if (age >= 5)
+     Console.WriteLine("You are allowed to watch a G-rated movie alone.");
+   else 
+     Console.WriteLine("You are too young to watch movies alone.");
 
-    Console.WriteLine("/nDone.");
+   Console.WriteLine("/nDone.");
   }
-} 
+}
